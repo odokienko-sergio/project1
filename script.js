64fa04c96c5497bc408b4b065f3263bcff02e513
Sergio <odockiencko.s@gmail.com>
2022-05-02 18:09:23 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,51 @@
+"use strict";
+
+console.log('arr' + " - object");
+console.log(4 + "5");
+
+let incr = 10,
+    decr = 10;
+
+//++incr;
+//--decr;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+
+console.log(2 + 2 * 2 !== '6');
+
+
+const isChecked = false,
+      isClose = false;
+
+console.log(isChecked || !isClose);
+
+/*
+const storeName = 'Bookshop';
+
+const storeDescription = {
+    budget: 10000,
+    employees: ["Johny", "Jim", "Jack"],
+    products: {
+        book: 300,
+        vinyl: 450
+    },
+    open: true
+};
+*/
+
+
+
+
+// snake_case
+// UPPER_SNAKE_CASE
+// Kebab-case
+// PascalCase
+
+const COLOR_RED = '#F00'; /* не нужно менять значение константы */
+
+const _apiBase = 'https://football.ua/';
+// ЗДЕСЬ БУДЕТ КЛЮЧ. ЭТОТ КЛЮЧ МОЖЕТ НЕ РАБОТАТЬ
+const _apiKey = 'apikey=930r9049039403';
