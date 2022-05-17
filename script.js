77edf2966f2657b1f9d763a6a3caebfe3e4d385d
Sergio <odockiencko.s@gmail.com>
2022-05-17 22:05:24 +0300

    if...else..switch..case

--- script.js
+++ script.js
@@ -20,6 +20,51 @@ personalMovieDB.movies[c] = d;
 
 console.log(personalMovieDB);
 
+if (3) {
+    console.log('Ok!');
+} else {
+    console.log('Error');
+}
+
+
+/*const num = 50;
+
+if (num < 49) {
+    console.log('Error');
+} else if (num > 100) {
+    console.log('Mnogo');
+} else {
+    console.log('Ok!');
+}*/
+
+/* Тернарный оператор ? - */
+/*(num === 50) ? console.log('Ok!') : console.log('Error');*/
+/* 4 + 4 Бинарный аргумент */
+/* +'4' Унарный аргумент*/
+
+
+const num = 50; 
+
+switch (num) {
+    case 49:
+        console.log('Neverno');
+        break;
+    case 100:
+        console.log('Neverno');
+        break;
+    case 51:
+        console.log('V tochku!');
+        break;
+    default:
+        console.log('Ne v etot raz');
+        break;
+}
+
+
+
+
+
+
 /*
 const storeName = 'Bookshop';
 
